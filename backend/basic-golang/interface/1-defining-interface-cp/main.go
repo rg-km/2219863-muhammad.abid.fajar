30043dbb458331265dc1e4139b155cb3f0645c08
Beddok <abidfajar50@gmail.com>
2022-03-29 12:01:24 +0700

    chore: submit interface 2

--- backend/basic-golang/interface/1-defining-interface-cp/main.go
+++ backend/basic-golang/interface/1-defining-interface-cp/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // Dari contoh yang telah diberikan, kamu dapat mencoba untuk membuat interface dan mengimplementasikan interface.
 // Buatlah interface Employee yang memiliki method signature GetBonus() int
 // Buatlah implementasi interface Employee pada objek Manager, SeniorEngineer, dan JuniorEngineer.
@@ -13,9 +11,24 @@ import "fmt"
 
 // TODO: answer here
 
+type EmployeeSearcher interface {
+	FindByName(name string) (Employee, error)
+	FindByOrigin(BaseSalary int) ([]Employee, error)
+}
+
+type Employee struct {
+	Name       string
+	BaseSalary int
+}
+
+type EmployeeSearcherWithMap struct {
+	data map[int]Employee
+}
+
 func TotalEmployeeBonus(employees []Employee) int {
 	// Hitunglah total bonus yang dikeluarkan dari list of Employee
 	// TODO: answer here
+	return 0
 }
 
 func main() {
--- backend/basic-golang/interface/1-defining-interface-cp/main_test.go
+++ backend/basic-golang/interface/1-defining-interface-cp/main_test.go
@@ -9,43 +9,43 @@ var _ = Describe("Employee", func() {
 	Describe("Get Manager Bonus", func() {
 		It("returns 3 times base salary", func() {
 			manager := Manager{
-				BaseSalary: 20000000,
+				BaseSalary: 20_000_000,
 			}
 			bonus := manager.GetBonus()
-			Expect(bonus).To(Equal(3 * 20000000))
+			Expect(bonus).To(Equal(3 * 20_000_000))
 		})
 	})
 	Describe("Get Senior Engineer Bonus", func() {
 		It("returns 2 times base salary", func() {
 			seniorEngineer := SeniorEngineer{
-				BaseSalary: 15000000,
+				BaseSalary: 15_000_000,
 			}
 			bonus := seniorEngineer.GetBonus()
-			Expect(bonus).To(Equal(2 * 15000000))
+			Expect(bonus).To(Equal(2 * 15_000_000))
 		})
 	})
 	Describe("Get Junior Engineer Bonus", func() {
 		It("returns base salary", func() {
 			juniorEngineer := JuniorEngineer{
-				BaseSalary: 10000000,
+				BaseSalary: 10_000_000,
 			}
 			bonus := juniorEngineer.GetBonus()
-			Expect(bonus).To(Equal(10000000))
+			Expect(bonus).To(Equal(10_000_000))
 		})
 	})
 	Describe("Get Total Employee Bonus", func() {
 		It("returns sum of employee bonus", func() {
 			manager := Manager{
-				BaseSalary: 20000000,
+				BaseSalary: 20_000_000,
 			}
 			seniorEngineer := SeniorEngineer{
-				BaseSalary: 15000000,
+				BaseSalary: 15_000_000,
 			}
 			juniorEngineer := JuniorEngineer{
-				BaseSalary: 10000000,
+				BaseSalary: 10_000_000,
 			}
 			totalBonus := TotalEmployeeBonus([]Employee{manager, seniorEngineer, juniorEngineer})
-			Expect(totalBonus).To(Equal(100000000))
+			Expect(totalBonus).To(Equal(100_000_000))
 		})
 	})
 })
