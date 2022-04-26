8c7709cfeec67145a42e682d0fe08147f2c43382
Beddok <abidfajar50@gmail.com>
2022-04-26 10:22:02 +0700

    submit

--- backend/concurrency/assignment/load-balancer-cp/main.go
+++ backend/concurrency/assignment/load-balancer-cp/main.go
@@ -20,12 +20,15 @@ func worker(in <-chan *Work, out chan<- *Work, number int) {
 	for w := range in {
 		w.data = fmt.Sprintf("worker %d accepted %s", number, w.data)
 		// TODO: answer here
+		// fmt.Printf(w.data)
+		out <- &Work{w.data}
 	}
 }
 
 func createRequest(in chan<- *Work, number int) {
 	for {
 		// TODO: answer here
+		in <- &Work{data: fmt.Sprintf("request from client %d", number)}
 	}
 }
 
@@ -35,8 +38,14 @@ func receiver(out <-chan *Work) {
 		//ke dalam map `data`
 		//gunakan mutex untuk mengamankan penulisan ke map secara concurrent
 
-		<-out // TODO: replace this
-		<-out
+		// <-out // TODO: replace this
+
 		// TODO: answer here
+
+		w := <-out
+		mu.Lock()
+		data[w.data] = true
+		mu.Unlock()
+		<-out
 	}
 }
