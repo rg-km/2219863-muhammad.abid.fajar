368728e92cc31133309378751f72cfd7a2fa38de
Beddok <abidfajar50@gmail.com>
2022-04-27 22:19:06 +0700

    clean folder

--- backend/auth/workshop/06-jwt-authorization/main.go
+++ backend/auth/workshop/06-jwt-authorization/main.go
@@ -23,31 +23,61 @@ func Routes() *http.ServeMux {
 	mux.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
 		var creds Credentials
 		// JSON body diconvert menjadi creditial struct & return bad request ketika terjadi kesalahan decoding json
-
 		// TODO: answer here
+		err := json.NewDecoder(r.Body).Decode(&creds)
+		if err != nil {
+			// return bad request ketika terjadi kesalahan decoding json
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
 		// Task: Ambil password dari username yang dipakai untuk login & return unauthorized jika password salah
-
 		// TODO: answer here
+		expectedPassword, ok := users[creds.Username]
+		if !ok || expectedPassword.Password != creds.Password {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("user credential invalid"))
+			return
+		}
 
 		// Task: 1. Deklarasi expiry time untuk token jwt
 		// 		 2. Buat claim menggunakan variable yang sudah didefinisikan diatas
 		//       3. Expiry time menggunakan time millisecond
 
 		// TODO: answer here
+		expirationTime := time.Now().Add(5 * time.Minute)
 
 		// Task: Buat token menggunakan encoded claim dengan salah satu algoritma yang dipakai
-
 		// TODO: answer here
+		claims := &Claims{
+			Username: creds.Username,
+			Role:     expectedPassword.Role,
+			StandardClaims: jwt.StandardClaims{
+				// expiry time menggunakan time millisecond
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
 
 		// Task: 1. Buat jwt string dari token yang sudah dibuat menggunakan JWT key yang telah dideklarasikan
 		//       2. Buat return internal error ketika ada kesalahan ketika pembuatan JWT string
 
 		// TODO: answer here
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// return internal error ketika ada kesalahan ketika pembuatan JWT string
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
 		// Task: Set token string kedalam cookie response
-
 		// TODO: answer here
+		http.SetCookie(w, &http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
+		w.Write([]byte("Login Success"))
 	})
 
 	mux.HandleFunc("/admin", func(w http.ResponseWriter, r *http.Request) {
