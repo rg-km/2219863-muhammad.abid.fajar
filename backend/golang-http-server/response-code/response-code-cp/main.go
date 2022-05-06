5460bf1c806d9c7787ce8197ff1c2f130f469364
Beddok <abidfajar50@gmail.com>
2022-05-06 13:12:49 +0700

    chore: submit

--- backend/golang-http-server/response-code/response-code-cp/main.go
+++ backend/golang-http-server/response-code/response-code-cp/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -25,11 +24,25 @@ func IsNameExists(name string) bool {
 func GetNameHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO: answer here
+		if r.Method == "POST" {
+			if r.FormValue("name") == "Roger" {
+				w.WriteHeader(405)
+				return
+			}
+		} else if r.Method == "GET" {
+			if r.FormValue("name") == "Roger" {
+				w.WriteHeader(http.StatusOK)
+			} else {
+				w.WriteHeader(404)
+			}
+		}
+
 	}
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	// TODO: answer here
+	mux.HandleFunc("/", GetNameHandler())
 	return mux
 }
