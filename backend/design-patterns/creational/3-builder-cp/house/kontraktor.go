cca8de83a230f4d8228eff6fd63b9fe0daa158f3
Beddok <abidfajar50@gmail.com>
2022-05-12 13:35:29 +0700

    submit

--- backend/design-patterns/creational/3-builder-cp/house/kontraktor.go
+++ backend/design-patterns/creational/3-builder-cp/house/kontraktor.go
@@ -8,16 +8,26 @@ type kontraktor struct {
 func NewKontraktor(builder HouseBuilder) *kontraktor {
 	return &kontraktor{
 		// TODO: answer here
+		builder: builder,
 	}
 }
 
 func (d *kontraktor) BuildHouse() House {
 	// TODO: answer here
+	d.builder.buildWindow(5)
+	d.builder.buildDoor()
+	d.builder.buildGarage()
+	d.builder.buildSwimmingPool()
+
 	return d.builder.getHouse()
 }
 
 // Rumah yang tidak memiliki SwimmingPool hanya boleh mimiliki satu jendela
 func (d *kontraktor) BuildHouseWithoutSwimmingPool() House {
 	// TODO: answer here
+	d.builder.buildWindow(1)
+	d.builder.buildDoor()
+	d.builder.buildGarage()
+
 	return d.builder.getHouse()
 }
