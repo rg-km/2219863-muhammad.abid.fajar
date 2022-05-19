8886bd293fda89f9c5a442c0289c462a58bfc594
Beddok <abidfajar50@gmail.com>
2022-05-19 10:03:30 +0700

    cp

--- backend/database/intro-to-sql/4-delete-cp/main.go
+++ backend/database/intro-to-sql/4-delete-cp/main.go
@@ -18,6 +18,7 @@ func (r *EmployeeRepository) DeleteEmployeeByNIK(nik string) error {
 	// 2. use nik to delete employee
 
 	// TODO: answer here
+	sqlStmt = `DELETE FROM employees WHERE nik = ?`
 
 	_, err := r.db.Exec(sqlStmt, nik)
 	if err != nil {
